28180fcc9156a9480975bb0be9a28e12ee83ba87
alfa <akhmadrofai@gmail.com>
2020-09-22 11:53:05 +0700

    fix scss and js

--- public/js/app.js
+++ public/js/app.js
@@ -49897,8 +49897,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/alfa/Laravel/base-admin-starter/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/alfa/Laravel/base-admin-starter/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/alfa/Playground/Laravel/wb-v3/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/alfa/Playground/Laravel/wb-v3/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
